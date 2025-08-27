d17757d7326c1bbd2f596d5d2a3420a4bbbd9352
Aniket Sharma <aniketsharma2914@gmail.com>
2025-08-27 11:41:44 +0530

    [AniketSharma].modified Assignment Code'

--- Assignment/Assignment-27th August/DateComparison.java
+++ Assignment/Assignment-27th August/DateComparison.java
@@ -7,19 +7,13 @@ public class DateComparison {
     }
 
     static void compareTwoDates() {
-        Scanner scanner = new Scanner(System.in);
-
-
+            Scanner scanner = new Scanner(System.in);
             System.out.print("(YYYY-MM-DD): ");
             String strdate = scanner.nextLine();
             LocalDate date1 = LocalDate.parse(strdate);
-
-
             System.out.print("(YYYY-MM-DD): ");
             String strdate2 = scanner.nextLine();
             LocalDate date2 = LocalDate.parse(strdate2);
-
-
             if (date1.isBefore(date2)) {
                 System.out.println(" date is BEFORE");
             } else if (date1.isAfter(date2)) {
@@ -27,7 +21,5 @@ public class DateComparison {
             } else if (date1.isEqual(date2)) {
                 System.out.println("Both dates are EQUAL.");
             }
-
-
-    }
+        }
 }
